0773868e721690e12f504aa5aa8b0046d39cf4cc
Richard Kurwitz <kurwitz3@aol.com>
2020-08-17 23:57:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,7 +24,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  print  array.index(index_number)
+ array[index_number].index
 end
 
 def retrieve_first_element_from_array(array)
